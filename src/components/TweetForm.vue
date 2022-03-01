aa21eef6c8d53753868073cea1bf4fbfcb9ca964
Alexis Mansilla <alexis.mansilla04@gmail.com>
2022-02-28 23:30:06 -0300

    Send form data.

--- src/components/TweetForm.vue
+++ src/components/TweetForm.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="tweet-form" :class="{ open: showForm }">
-    <form>
-      <input class="form-control" placeholder="Tu nombre"/>
-      <textarea class="form-control" row="3" placeholder="Escribe el tweet"></textarea>
+    <form @submit.prevent="sendTweet">
+      <input class="form-control" placeholder="Tu nombre" v-model="username"/>
+      <textarea class="form-control" row="3" placeholder="Escribe el tweet" v-model="tweet"></textarea>
       <button type="submit" class="btn btn-primary">Enviar Tweet</button>
     </form>
   </div>
 </template>
+
 <script>
+import { ref } from "vue"
+
 export default {
   props: {
     showForm: Boolean
+  },
+  setup() {
+    let username = ref("")
+    let tweet = ref("")
+
+    const sendTweet = () => {
+      console.log("enviando formulario")
+      console.log(username.value)
+      console.log(tweet.value)
+    };
+
+    return { sendTweet, username, tweet }
   }
 }
 </script>
