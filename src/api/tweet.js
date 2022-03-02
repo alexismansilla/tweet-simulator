a86ad0d68c77183e45f60f31a211ae74e73d3de2
Alexis Mansilla <alexis.mansilla04@gmail.com>
2022-03-02 16:47:45 -0300

    Add more tweets on localstorage.

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -2,12 +2,36 @@ import { v4 as uuidv4 } from "uuid";
 import { TWEET } from '../utils/constant'
 
 export function saveTweetApi(tweet, username) {
-  const tweetTemp = {
-    id: uuidv4(),
-    tweet: tweet,
-    username: username,
-    created_at: new Date()
+  const tweets = getTweetApi()
+
+  if(tweets.length === 0) {
+    const tweetTemp = [{
+      id: uuidv4(),
+      tweet: tweet,
+      username: username,
+      created_at: new Date()
+    }]
+
+    localStorage.setItem(TWEET, JSON.stringify(tweetTemp))
+  }else {
+    tweets.push({
+      id: uuidv4(),
+      tweet,
+      username,
+      created_at: new Date()
+    })
+
+    localStorage.setItem(TWEET, JSON.stringify(tweets))
   }
 
-  localStorage.setItem(TWEET, JSON.stringify(tweetTemp))
+}
+
+export function getTweetApi() {
+  const tweets = localStorage.getItem(TWEET)
+
+  if(tweets) {
+    return JSON.parse(tweets)
+  }
+
+  return []
 }
